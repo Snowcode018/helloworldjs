6339cf5f2a09b64123640fcdf25e7e5e9f6c36ad
Snowcode018 <artchaser6@gmail.com>
2024-09-16 21:50:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-
+/*
 public class Hello {
     public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
 }
 }
+*/
+
+document.getElementById('app').innerHTML = 
+    <h1> 
+        'Hello Cruel World'
+    </h1>;
